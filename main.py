13bbd5236a902519f33e03f7db6eceec95df777b
Ante-237 <akumawahanye@gmail.com>
2022-06-25 10:25:24 +0200

    changes to game_data.py

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 # ROCK PAPER SCISSORS
 
-import func_try as trying
-
 name = "Rock paper Scissors"
 
 
 def run_game():
-    print(name)
-    trying.use_list(0)
-    temp_name = trying.return_to_list(2)
-    print(temp_name)
-    print(trying.player_Age)
-    print(trying.player_name)
+   print('Test Area')
 
 
 run_game()
